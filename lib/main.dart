aa574e7859879908e16211332358cc91c2e76a8e
Ram Suthar <ramsuthar305@gmail.com>
2023-12-17 03:17:50 +0530

    reverted home screen
    Signed-off-by: Ram Suthar <ramsuthar305@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MyApp extends StatelessWidget {
           fontFamily: 'Inter',
           useMaterial3: true,
         ),
-        home: const QuizQuestion());
+        home: const SplashManager());
   }
 }
 

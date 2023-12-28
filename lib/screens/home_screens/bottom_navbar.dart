0449d05a16c52bc77aba37113ecc5ddefd190c52
durgeshahire07 <durgeshahire07@gmail.com>
2023-12-28 19:40:59 +0530

    feat: :sparkles: added icons

--- lib/screens/home_screens/bottom_navbar.dart
+++ lib/screens/home_screens/bottom_navbar.dart
@@ -6,21 +6,24 @@ import '../create_challenge/ui/create_challenge_screen.dart';
 class BottomNavigationBarWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Positioned(
-        bottom: 100,
-        left: 80,
-        right: 80,
+    return Container(
+        margin: EdgeInsets.only(bottom: 20),
+        // bottom: 100,
+        // left: 80,
+        // right: 80,
         height: 50,
         child: Container(
           height: 50,
-          width: 70,
+          width: 240,
           decoration: const BoxDecoration(
               color: Colors.black,
               borderRadius: BorderRadius.all(Radius.circular(100))),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
-              SvgPicture.asset('assets/images/home.svg'),
+              SvgPicture.asset(
+                'assets/images/home.svg',
+              ),
               Padding(
                   padding: const EdgeInsets.only(
                       left: 8, top: 4, right: 0, bottom: 4),
